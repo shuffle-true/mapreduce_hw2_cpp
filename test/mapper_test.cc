e4a31996f9c6040332f9c544005181885add5ea2
akrozhin <akrozhin@miem.hse.ru>
2023-05-19 15:44:12 +0300

    Add split && map tests

--- test/mapper_test.cc
+++ test/mapper_test.cc
@@ -2,39 +2,119 @@
 // Created by rozhin on 18.05.23.
 //
 
-#include "job/job.h"
+#include "test_utils.h"
 #include <gtest/gtest.h>
 using namespace mapreduce;
 
-class MapTask : public map_task_base {
-public:
-    typedef const char* key_type;
-    typedef size_t      value_type;
+void mapper(const char* key, const size_t value, mapreduce::pair_vec& result) {
+    const char* pos = key;
+    while(pos - key < value) {
+        const char* start_pos = pos;
+        while (pos - key < value && *pos != ' ') {
+            pos += 1;
+        }
+        result.push_back(std::string_view(start_pos, pos - start_pos));
+        pos += 1;
+    }
+}
 
-public:
-    void operator()(const char* key, const size_t value, pair_vec& result) {
-        for (size_t i = 0; i < value; ++i) {
-            result.push_back(std::string_view (key, value));
+void reducer(mapreduce::map_vec& mapper_output, std::map<std::string_view, int32_t>& result, std::string_view key){
+    result.insert({key, 0});
+    for(size_t i=0; i<mapper_output.size(); i++){
+        for(size_t j=0; j < mapper_output[i].size(); j++){
+            if (mapper_output[i][j]==key){
+                result[key]++;
+            }
         }
     }
+}
 
-};
-
-TEST(mapper, vanilla_1) {
+TEST(jobber, check_in_splits) {
     std::vector<std::string> filenames = {
-            "1.txt",
-            "2.txt",
-            "3.txt",
-            "4.txt"
+            "../../test/1.txt",
+            "../../test/2.txt"
     };
-
-    MapTask mapper;
 
     mapreduce::JobContext context(
             filenames,
-            12, 12,
-            ".", ".", &mapper);
+            12, 4,
+            ".", ".", mapper, reducer);
 
-    mapreduce::job job(context);
-    job.start();
+    mapreduce::JobTester tester(context);
+    tester.test_splits_routine();
+    auto in_splits = tester.in_splits();
+    ASSERT_EQ(tester.in_splits_size(), 11);
+
+    ASSERT_EQ(std::string_view(in_splits[0].first, in_splits[0].second), "I try");
+    ASSERT_EQ(std::string_view(in_splits[1].first, in_splits[1].second), "To understand");
+    ASSERT_EQ(std::string_view(in_splits[2].first, in_splits[2].second), "What the difference");
+    ASSERT_EQ(std::string_view(in_splits[3].first, in_splits[3].second), "Between");
+    ASSERT_EQ(std::string_view(in_splits[4].first, in_splits[4].second), "Me");
+    ASSERT_EQ(std::string_view(in_splits[5].first, in_splits[5].second), "And");
+    ASSERT_EQ(std::string_view(in_splits[6].first, in_splits[6].second), "You");
+    ASSERT_EQ(std::string_view(in_splits[7].first, in_splits[7].second), "Ok");
+    ASSERT_EQ(std::string_view(in_splits[8].first, in_splits[8].second), "Answer");
+    ASSERT_EQ(std::string_view(in_splits[9].first, in_splits[9].second), "Is that you");
+    ASSERT_EQ(std::string_view(in_splits[10].first, in_splits[10].second), "A bullshit boy");
 }
+
+TEST(jobber, mapper) {
+    std::vector<std::string> filenames = {
+            "../../test/1.txt",
+            "../../test/2.txt"
+    };
+
+    mapreduce::JobContext context(
+            filenames,
+            12, 4,
+            ".", ".", mapper, reducer);
+
+    mapreduce::JobTester tester(context);
+    tester.test_map_routine();
+    auto mapper_res = tester.mapper_results();
+
+    ASSERT_EQ(mapper_res.size(), 11);
+
+    ASSERT_EQ(mapper_res[0].size(), 2);
+    ASSERT_EQ(mapper_res[0][0], "I");
+    ASSERT_EQ(mapper_res[0][1], "try");
+
+    ASSERT_EQ(mapper_res[1].size(), 2);
+    ASSERT_EQ(mapper_res[1][0], "To");
+    ASSERT_EQ(mapper_res[1][1], "understand");
+
+    ASSERT_EQ(mapper_res[2].size(), 3);
+    ASSERT_EQ(mapper_res[2][0], "What");
+    ASSERT_EQ(mapper_res[2][1], "the");
+    ASSERT_EQ(mapper_res[2][2], "difference");
+
+    ASSERT_EQ(mapper_res[3].size(), 1);
+    ASSERT_EQ(mapper_res[3][0], "Between");
+
+    ASSERT_EQ(mapper_res[4].size(), 1);
+    ASSERT_EQ(mapper_res[4][0], "Me");
+
+    ASSERT_EQ(mapper_res[5].size(), 1);
+    ASSERT_EQ(mapper_res[5][0], "And");
+
+    ASSERT_EQ(mapper_res[6].size(), 1);
+    ASSERT_EQ(mapper_res[6][0], "You");
+
+    ASSERT_EQ(mapper_res[7].size(), 1);
+    ASSERT_EQ(mapper_res[7][0], "Ok");
+
+    ASSERT_EQ(mapper_res[8].size(), 1);
+    ASSERT_EQ(mapper_res[8][0], "Answer");
+
+    ASSERT_EQ(mapper_res[9].size(), 3);
+    ASSERT_EQ(mapper_res[9][0], "Is");
+    ASSERT_EQ(mapper_res[9][1], "that");
+    ASSERT_EQ(mapper_res[9][2], "you");
+
+    ASSERT_EQ(mapper_res[10].size(), 3);
+    ASSERT_EQ(mapper_res[10][0], "A");
+    ASSERT_EQ(mapper_res[10][1], "bullshit");
+    ASSERT_EQ(mapper_res[10][2], "boy");
+}
+
+// TODO: write test for shuffler && reducer
--- test/threadpool_test.cc
+++ test/threadpool_test.cc
@@ -1,4 +1,57 @@
 //
 // Created by rozhin on 13.05.23.
-// Copyright (c) 2021-2023 xsdnn. All rights reserved.
 //
+
+#include "concurrency/threadpool.h"
+#include "job/job.h"
+#include <gtest/gtest.h>
+using namespace mapreduce;
+
+void sum(int& ans, std::vector<int>& arr) {
+    for (int i = 0; i < arr.size(); ++i) {
+        ans += arr[i];
+    }
+}
+
+class MapTask2 {
+public:
+    void operator()(const char* key, const size_t value, pair_vec& result) {
+        for (size_t i = 0; i < value; ++i) {
+            result.push_back(std::string_view (key, value));
+        }
+    }
+};
+
+
+TEST(threadpool, _01) {
+     threadpool tp(3);
+     std::vector<int> s1 = { 1, 2, 3 };
+     int ans1 = 0;
+
+     std::vector<int> s2 = { 4, 5 };
+     int ans2 = 0;
+
+     std::vector<int> s3 = { 8, 9, 10 };
+     int ans3 = 0;
+
+     std::vector<int> s4 = { 8, 9, 10, 20 };
+     int ans4 = 0;
+
+     MapTask2 mt;
+     const char* a;
+     size_t b = 1;
+     pair_vec res;
+
+//      добавляем в thread_pool выполняться 3 задачи
+     auto id1 = tp.add_task(mt, std::ref(a), std::ref(b), std::ref(res));
+     auto id2 = tp.add_task(sum, std::ref(ans2), std::ref(s2));
+     auto id3 = tp.add_task(sum, std::ref(ans3), std::ref(s3));
+     auto id4 = tp.add_task(sum, std::ref(ans4), std::ref(s4));
+//
+//     tp.wait_all();
+//
+//
+//     std::cout << ans2 << std::endl;
+//     std::cout << ans3 << std::endl;
+//     std::cout << ans4 << std::endl;
+}
